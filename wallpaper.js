cb03fec35c2734cbbec9abf9c243619b45400e1e
Vivek Gupta <viv3kgupta@gmail.com>
2020-08-01 09:49:47 +0530

    first_website
    i like it

---
+++ wallpaper.js
@@ -0,0 +1,40 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('themedot')
+
+
+for (var i=0;i< themeDots.length ; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme_style').href = 'default.css'
+	}
+
+	if(mode == 'skyblue'){
+		document.getElementById('theme_style').href = 'sky_blue.css'
+	}
+
+	if(mode == 'yellowgreen'){
+		document.getElementById('theme_style').href = 'yellow_green.css'
+	}
+
+	if(mode == 'voilet'){
+		document.getElementById('theme_style').href = 'voilet.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
